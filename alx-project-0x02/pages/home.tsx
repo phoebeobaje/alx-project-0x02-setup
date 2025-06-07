67add8210a5eaa060be0053621431d4483537e32
phoebeobaje <phoeberobertscareer@gmail.com>
2025-06-07 15:43:14 +0100

    fixing card comp

--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -1,6 +1,14 @@
+import Card from "@/components/common/Card";
+
 const Home: React.FC = () => {
     return(
+        <>
         <h1>Home Page</h1>
+            <Card title="Card Title" content="This is a sample card content." />
+    <Card title="Another Card" content="This is another card with different content." />
+    <Card title="Third Card" content="This is the third card with its own content." />
+
+        </>
     )
 }
 export default Home;
